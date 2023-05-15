7a5d24495977cdece33897f32fd72f382a4bbb7c
bruno henrique <bh.bsb2011@hotmail.com>
2023-05-15 19:17:04 -0300

    update shortest

--- codes/shortest_path.py
+++ codes/shortest_path.py
@@ -4,15 +4,16 @@ from typing import List
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
         n = len(graph)
-        target_mask = (1 << n) - 1  # Binary mask representing all nodes visited
+        # Máscara binária representando todos os nós visitados
+        target_mask = (1 << n) - 1 
         
-        # Create a priority queue to store the states (node, mask, distance)
-        queue = [(0, i, 1 << i) for i in range(n)]  # (distance, node, mask)
+        # Cria uma fila de prioridade para armazenar os estados (nó, máscara, distância)
+        queue = [(0, i, 1 << i) for i in range(n)]
         
-        # Create a distance table to keep track of the minimum distances
+        # Cria uma tabela de distâncias para acompanhar as distâncias mínimas
         distance = [[float('inf')] * (1 << n) for _ in range(n)]
         
-        # Initialize the distance for starting nodes to 0
+        # Inicialize a distância para os nós iniciais em 0
         for i in range(n):
             distance[i][1 << i] = 0
         
@@ -33,4 +34,4 @@ class Solution:
                     distance[neighbor][new_mask] = new_dist
                     heapq.heappush(queue, (new_dist, neighbor, new_mask))
         
-        return -1  # No valid path found
+        return -1  # Nenhum caminho válido encontrado
